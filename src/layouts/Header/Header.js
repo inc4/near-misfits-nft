24fba6713d6417febb0cdf7976381fec13ecee91
Ivan <ikharchenko2008@gmail.com>
2021-11-15 18:02:23 +0200

    del unused logo in header

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,8 +1,5 @@
 import React, { useContext, memo } from 'react';
-import { ReactSVG } from 'react-svg';
-import { Link } from 'react-router-dom';
 import { appStore } from '../../state/app';
-import logo from '../../assets/images/nearkat-logo.svg';
 import Navigation from '../../components/Navigation';
 import SocialLinks from '../../components/SocialLinks';
 import ConnectWalletBtn from '../../components/ConnectWalletBtn';
@@ -18,9 +15,6 @@ const Header = () => {
 
   return (
     <header className="header">
-      <Link to="/">
-        <ReactSVG className="header__logo" src={logo} />
-      </Link>
       <Navigation className="header__navigation" signedIn={wallet.signedIn} />
       <SocialLinks className="header__social" />
       {account?.accountId ? (
