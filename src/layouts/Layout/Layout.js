42bc3933b4c0df33b7a8b7ce41f3536bf2bb502b
Ivan <ikharchenko2008@gmail.com>
2021-11-17 22:39:32 +0200

    fix scroll

--- src/layouts/Layout/Layout.js
+++ src/layouts/Layout/Layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useHistory } from 'react-router';
@@ -11,24 +12,15 @@ const Layout = ({ children }) => {
   useEffect(() => {
     if (!hash) return;
 
-    if (hash !== '#generate') history.push(pathname);
+    setTimeout(() => {
+      const elem = document.querySelector(hash);
 
-    if (hash !== '#share-nft') {
-      setTimeout(() => {
-        const elem = document.querySelector(hash);
-
-        elem.scrollIntoView({
-          behavior: 'smooth',
-        });
-      }, 100);
-    } else {
-      setTimeout(() => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
+      elem?.scrollIntoView({
+        behavior: 'smooth',
       });
-    }
+    }, 100);
+
+    if (hash !== '#generate-block') history.push(pathname);
   }, [hash]);
 
   return (
