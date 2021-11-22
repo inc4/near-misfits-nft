c751fa1c3becbdbdd85dc8d24bafe2c424c8ae21
Ivan <ikharchenko2008@gmail.com>
2021-11-22 20:40:07 +0200

    refactor component

--- src/components/RateList/RateList.jsx
+++ src/components/RateList/RateList.jsx
@@ -1,16 +1,62 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import RateListItem from './RateListItem';
+import { ReactSVG } from 'react-svg';
+import { FormattedMessage } from 'react-intl';
+import normies from '../../assets/images/rarity-common.svg';
+import zombies from '../../assets/images/rarity-uncommon.svg';
+import skeletons from '../../assets/images/rarity-rare.svg';
+import nearkats from '../../assets/images/rarity-very-rare.svg';
+import glitch from '../../assets/images/rarity-super-rare.svg';
 
-const RateList = ({ rates }) => (
+const RateList = () => (
   <div className="rate-list">
-    {rates.length &&
-      rates.map((rate) => <RateListItem key={rate.id} rate={rate} />)}
+    <div className="rate-item">
+      <div className="rate-item__count">1,600</div>
+      <ReactSVG className="rate-item__icon" src={normies} />
+      <p className="rate-item__text">
+        <FormattedMessage
+          id="rarity.rateListNormies"
+          defaultMessage="Normies"
+        />
+      </p>
+    </div>
+    <div className="rate-item">
+      <div className="rate-item__count">150</div>
+      <ReactSVG className="rate-item__icon" src={zombies} />
+      <p className="rate-item__text">
+        <FormattedMessage
+          id="rarity.rateListZombies"
+          defaultMessage="Zombies"
+        />
+      </p>
+    </div>
+    <div className="rate-item">
+      <div className="rate-item__count">101</div>
+      <ReactSVG className="rate-item__icon" src={skeletons} />
+      <p className="rate-item__text">
+        <FormattedMessage
+          id="rarity.rateListSkeletons"
+          defaultMessage="Skeletons"
+        />
+      </p>
+    </div>
+    <div className="rate-item">
+      <div className="rate-item__count">50</div>
+      <ReactSVG className="rate-item__icon" src={nearkats} />
+      <p className="rate-item__text">
+        <FormattedMessage
+          id="rarity.rateListNearkats"
+          defaultMessage="Nearkats"
+        />
+      </p>
+    </div>
+    <div className="rate-item">
+      <div className="rate-item__count">22</div>
+      <ReactSVG className="rate-item__icon" src={glitch} />
+      <p className="rate-item__text">
+        <FormattedMessage id="rarity.rateListGlitch" defaultMessage="Glitch" />
+      </p>
+    </div>
   </div>
 );
-
-RateList.propTypes = {
-  rates: PropTypes.array,
-};
 
 export default RateList;
