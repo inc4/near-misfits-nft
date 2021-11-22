4f4bde61db54acf0e8ad79b26ee29f9676be3dfe
Ivan <ikharchenko2008@gmail.com>
2021-11-22 13:22:24 +0200

    add smooth scroll when push button

--- src/components/Hero/CalendarBtn.jsx
+++ src/components/Hero/CalendarBtn.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
-const CalendarBtn = ({ className }) => (
-  <a
-    href="/#generate"
-    title="Add to Calendar"
-    className={`calendar-btn  ${className}`}
-  >
-    Mint your first!
-  </a>
-);
+const CalendarBtn = ({ className }) => {
+  const history = useHistory();
+
+  return (
+    <button
+      type="button"
+      title="Add to Calendar"
+      className={`calendar-btn  ${className}`}
+      onClick={() => history.push('/#generate')}
+    >
+      Mint your first!
+    </button>
+  );
+};
 
 CalendarBtn.defaultProps = {
   className: '',
