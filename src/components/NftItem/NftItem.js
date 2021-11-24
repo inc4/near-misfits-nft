a0f93ff17ded6a03d3191fd2a5e6f26880e54692
Ivan <ikharchenko2008@gmail.com>
2021-11-25 00:28:11 +0200

    fix rarity icon and data

--- src/components/NftItem/NftItem.js
+++ src/components/NftItem/NftItem.js
@@ -1,14 +1,39 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactSVG } from 'react-svg';
 import PropTypes from 'prop-types';
 import NftItemInfo from '../NftItemInfo';
-import rarity from '../../assets/images/rarity-common.svg';
+import Normies from '../../assets/images/rarity-common.svg';
+import Skellies from '../../assets/images/rarity-rare.svg';
+import Zombies from '../../assets/images/rarity-uncommon.svg';
+import Meerkats from '../../assets/images/rarity-very-rare.svg';
+import Glitches from '../../assets/images/rarity-super-rare.svg';
 import Reveal from '../../pages/MyNFTS/Reveal';
+
+const rarity = {
+  Normies,
+  Skellies,
+  Zombies,
+  Meerkats,
+  Glitches,
+};
 
 const NftItem = ({ item, onClick, isReveal, urlIpfs }) => {
   const [reveal, setReveal] = useState(isReveal);
+  const [info, setInfo] = useState({});
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${urlIpfs}/${item.reference}`);
+      const data = await response.json();
+
+      setInfo(data);
+    } catch (e) {
+      // eslint-disable-next-line
+      console.log(e);
+    }
+  }, []);
 
   const handleClick = () => {
     setReveal(true);
@@ -27,13 +52,14 @@ const NftItem = ({ item, onClick, isReveal, urlIpfs }) => {
 
           <div className="nft-item__footer">
             <p className="nft-item__hash"># {item?.title?.padStart(5, '0')}</p>
-            <ReactSVG src={item.rarity || rarity} />
+            <ReactSVG src={rarity[info?.kind] || Normies} />
           </div>
 
           <NftItemInfo
             className="nft-item__info"
             item={item}
             urlIpfs={urlIpfs}
+            info={info}
           />
         </div>
       ) : (
