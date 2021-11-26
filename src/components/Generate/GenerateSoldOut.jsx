3f6b96dfab35d45ee247eed3999a12b65b9d4443
Ivan <ikharchenko2008@gmail.com>
2021-11-26 08:26:17 +0200

    add SendNft for sold out

--- src/components/Generate/GenerateSoldOut.jsx
+++ src/components/Generate/GenerateSoldOut.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BuyMoreBtn from '../BuyMoreBtn';
+import useTransfer from '../../hooks/useTransfer';
 
-const GenerateSoldOut = ({ className }) => (
-  <div className={`generate-sold-out ${className || ''}`}>
-    <p className="generate-sold-out__text">Sold Out</p>
-    <a
-      className="generate-sold-out__link"
-      href="https://paras.id/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <p>Buy on Paras</p>
-      <picture>
-        <source srcSet="./images/generate-paras-2x.webp 2x, ./images/generate-paras.webp 1x" />
-        <img
-          src="./images/generate-paras.png"
-          alt="paras marketplace icon"
-          className="generate-sold-out__img"
-        ></img>
-      </picture>
-    </a>
-  </div>
-);
+const GenerateSoldOut = ({ className }) => {
+  const { nftTransfer } = useTransfer();
 
+  return (
+    <div className={`generate-sold-out ${className || ''}`}>
+      <p className="generate-sold-out__text">Sold Out</p>
+      <a
+        className="generate-sold-out__link"
+        href="https://paras.id/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p>Buy on Paras</p>
+        <picture>
+          <source srcSet="./images/generate-paras-2x.webp 2x, ./images/generate-paras.webp 1x" />
+          <img
+            src="./images/generate-paras.png"
+            alt="paras marketplace icon"
+            className="generate-sold-out__img"
+          ></img>
+        </picture>
+      </a>
+      <BuyMoreBtn
+        text="Send an NFT"
+        className="buy__nft"
+        onClick={nftTransfer}
+      />
+    </div>
+  );
+};
 GenerateSoldOut.propTypes = {
   className: PropTypes.string,
 };
